8cac8ea7b8890112012c477e0bce25a9dffbdfa9
Valentina Pereyra Castillo <valepereyracastillo@gmail.com>
2023-02-03 23:21:40 +0000

    Hice models de  Costumer, Category, Product, Favorites, Related_Product, Order_Status_Code, Order, Order_Item, Cart. FALTA CORREGIR UNA RELACION DE ORDER

--- src/api/commands.py
+++ src/api/commands.py
@@ -1,6 +1,6 @@
 
 import click
-from api.models import db, User
+from api.models import db, Costumer
 
 """
 In this file, you can add as many commands as you want using the @app.cli.command decorator
@@ -10,23 +10,23 @@ with youy database, for example: Import the price of bitcoin every night as 12am
 def setup_commands(app):
     
     """ 
-    This is an example command "insert-test-users" that you can run from the command line
-    by typing: $ flask insert-test-users 5
-    Note: 5 is the number of users to add
+    This is an example command "insert-test-costumers" that you can run from the command line
+    by typing: $ flask insert-test-costumers 5
+    Note: 5 is the number of costumers to add
     """
-    @app.cli.command("insert-test-users") # name of our command
+    @app.cli.command("insert-test-costumers") # name of our command
     @click.argument("count") # argument of out command
     def insert_test_data(count):
-        print("Creating test users")
+        print("Creating test costumers")
         for x in range(1, int(count) + 1):
-            user = User()
-            user.email = "test_user" + str(x) + "@test.com"
-            user.password = "123456"
-            user.is_active = True
-            db.session.add(user)
+            costumer = Costumer()
+            costumer.email = "test_costumer" + str(x) + "@test.com"
+            costumer.password = "123456"
+            costumer.is_active = True
+            db.session.add(costumer)
             db.session.commit()
-            print("User: ", user.email, " created.")
+            print("Costumer: ", costumer.email, " created.")
 
-        print("All test users created")
+        print("All test costumers created")
 
         ### Insert the code to populate others tables if needed
--- src/api/models.py
+++ src/api/models.py
@@ -2,18 +2,195 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Costumer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    username = db.Column(db.String(80), unique=False, nullable=False)
+    address = db.Column(db.String(200), unique=False, nullable=False)
+    role = db.Column(db.String(80), unique=False, nullable=False)
+    phone = db.Column(db.String(80), unique=False, nullable=False)
+    favorites = db.relationship('Favorites', backref='costumer', lazy=True)
+    product = db.relationship('Product', backref='costumer', lazy=True)
+    order = db.relationship('Order', backref='costumer', lazy=True)
+    cart = db.relationship('Cart', backref='costumer', lazy=True)
+
 
     def __repr__(self):
-        return f'<User {self.email}>'
+        return f'<Costumer {self.id}>'
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "username": self.username,
+            "address": self.address,
+            "role": self.role,
+            "phone": self.phone,
             # do not serialize the password, its a security breach
+        }
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    code = db.Column(db.String(80), unique=False, nullable=False)
+    product = db.relationship('Product', backref='category', lazy=True)
+    related = db.relationship('Related_Product', backref='category', lazy=True)
+    
+
+    def __repr__(self):
+        return f'<Category {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "code": self.code,
+        }
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=False, nullable=False)
+    sku = db.Column(db.Integer, unique=False, nullable=False)
+    description = db.Column(db.String(120), unique=False, nullable=False)
+    image = db.Column(db.String(120), unique=False, nullable=False)
+    price = db.Column(db.Integer, unique=False, nullable=False)
+    costumer_id = db.Column(db.Integer, db.ForeignKey('costumer.id'), nullable=True)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=True)
+    favorites = db.relationship('Favorites', backref='product', lazy=True)
+    related_product = db.relationship('Related_Product', backref='product', lazy=True)
+    order_item = db.relationship('Order_Item', backref='product', lazy=True)
+    cart = db.relationship('Cart', backref='product', lazy=True)
+    
+
+    
+    def __repr__(self):
+        return f'<Products {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "sku": self.sku,
+            "description": self.description,
+            "image": self.image,
+            "price": self.price,
+            "costumer_id": self.costumer_id,
+            "category_id": self.category_id
+        }
+
+class Favorites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=True)
+    costumer_id = db.Column(db.Integer, db.ForeignKey('costumer.id'), nullable=True)
+
+
+
+    def __repr__(self):
+        return f'<Favorites {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "product_id": self.product_id,
+            "costumer_id": self.costumer_id
+        }
+
+class Related_Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=True)
+    related_product = db.Column(db.String(120), unique=False, nullable=False) #revisar relacion
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=True)
+
+
+
+    def __repr__(self):
+        return f'<Related_Product {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "product_id": self.product_id,
+            "related_product": self.related_product,
+            "category_id": self.category_id
+        }
+
+class Order_Status_Code(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    status_code = db.Column(db.Integer, unique=False, nullable=False)
+    description = db.Column(db.String(120), unique=False, nullable=False)
+    # order = db.relationship('Order', backref='order_status_code', lazy=True)
+    
+    def __repr__(self):
+        return f'<Order_Status_Code {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "status_code": self.status_code,
+            "description": self.description,
+        }
+
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    # order_status_code_id = db.Column(db.Integer, db.ForeignKey('order_status_code.id'), nullable=True) NO FUNCIONA
+    costumer_id = db.Column(db.Integer, db.ForeignKey('costumer.id'), nullable=True)
+    order_item = db.relationship('Order_Item', backref='order', lazy=True)
+
+
+    def __repr__(self):
+        return f'<Order {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            # "order_status_code_id": self.order_status_code_id,
+            "costumer_id": self.costumer_id,
+        }
+
+
+class Order_Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    order_id = db.Column(db.Integer, db.ForeignKey('order.id'), nullable=True)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=True)
+    quantity = db.Column(db.Integer, unique=False, nullable=False)
+    price = db.Column(db.Integer, unique=False, nullable=False)
+    
+
+
+    def __repr__(self):
+        return f'<Order_Item {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "order_id": self.order_id,
+            "product_id": self.product_id,
+            "quantity": self.quantity,
+            "price": self.price
+            
+        }
+
+
+class Cart(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    costumer_id = db.Column(db.Integer, db.ForeignKey('costumer.id'), nullable=True)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=True)
+    quantity = db.Column(db.Integer, unique=False, nullable=False)
+    price = db.Column(db.Integer, unique=False, nullable=False)
+    
+
+
+    def __repr__(self):
+        return f'<Cart {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "costumer_id": self.costumer_id,
+            "product_id": self.product_id,
+            "quantity": self.quantity,
+            "price": self.price
+            
         }
