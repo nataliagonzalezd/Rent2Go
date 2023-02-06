e65affe8cd47b56f2cfe5d3d799fd9f3bc3a6a2f
PabloAbreu1 <pagustin2404@gmail.com>
2023-02-06 17:10:42 +0000

    El back del register

--- src/app.py
+++ src/app.py
@@ -81,8 +81,6 @@ def login():
     if email != costumer.email or password != costumer.password:
         return jsonify({"msg": "Bad email or password"}), 401
 
-
-    
     access_token = create_access_token(identity=email)
     return jsonify(access_token=access_token)
 
