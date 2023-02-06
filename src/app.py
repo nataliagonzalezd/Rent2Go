de4e0ed9a22029eb0498ea599d83400a26e80986
PabloAbreu1 <pagustin2404@gmail.com>
2023-02-06 14:54:48 +0000

    back del register

--- src/app.py
+++ src/app.py
@@ -81,6 +81,20 @@ def login():
     if email != costumer.email or password != costumer.password:
         return jsonify({"msg": "Bad email or password"}), 401
 
+@app.route('/costumer', methods=['POST'])
+def crear_Usuario():
+    
+    request_body = request.json #Guardo la respuesta que trae la solicitud en una variable que se llama "request_body" que es un objeto
+    print(request_body)
+    get_costumer = Costumer.query.filter_by(email = request_body["email"]).first() #Filtro User para que me diga si este email ya esta registrado
+    if get_costumer is None: #si no esta registrado que cree uno
+        crear_nuevo_usuario = User(email= request_body["email"] , username = request_body["username"], password= request_body["password"], is_active= request_body["is_active"])
+        db.session.add(crear_nuevo_usuario)
+        db.session.commit()
+        return jsonify({"msg":"Nuevo usuario creado con exito"}), 200
+    else:
+        return jsonify({"msg":"El email ya existe"}), 400
+
     
     access_token = create_access_token(identity=email)
     return jsonify(access_token=access_token)
