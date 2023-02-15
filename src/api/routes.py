213ccccf5975d4b0f031da763ebc7595280c1088
JairzinhoTorres <jair1924torres@gmail.com>
2023-02-15 20:32:15 +0000

    Orden

--- src/api/routes.py
+++ src/api/routes.py
@@ -11,17 +11,23 @@ from flask_jwt_extended import JWTManager
 from flask_mail import Message
 import random
 import string
+
 api = Blueprint('api', __name__)
 # Handle/serialize errors like a JSON object
+
 @api.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_cod
+
+# any other endpoint will try to serve it like a static file
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
     response_body = {
         "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
     }
     return jsonify(response_body), 200
+
+# any other endpoint will try to serve it like a static file
 @api.route('/product', methods=['POST'])
 def add_new_product():
     request_body = request.json
@@ -32,6 +38,8 @@ def add_new_product():
     db.session.add(new_product)
     db.session.commit()
     return jsonify({"msg":"Producto creado correctamente"}),200
+
+# any other endpoint will try to serve it like a static file
 @api.route('/editprofile', methods=['POST'])
 def edit_profile():
     request_body = request.json
@@ -39,12 +47,16 @@ def edit_profile():
     db.session.add(edit_profile)
     db.session.commit()
     return jsonify({"msg":"Datos de profile obtenidos de forma satisfactoria"}),200
+
+
+# -----------------------------------------------------------endpoints --------------------------------- #
     # generate sitemap with all your endpoints
 @api.route('/')
 def sitemap():
     if ENV == "development":
         return generate_sitemap(app)
     return send_from_directory(static_file_dir, 'index.html')
+
 # any other endpoint will try to serve it like a static file
 @api.route('/<path:path>', methods=['GET'])
 def serve_any_other_file(path):
@@ -53,6 +65,8 @@ def serve_any_other_file(path):
     response = send_from_directory(static_file_dir, path)
     response.cache_control.max_age = 0 # avoid cache memory
     return response
+
+# any other endpoint will try to serve it like a static file
 @api.route("/login", methods=["POST"])
 def login():
     email = request.json.get("email", None)
@@ -62,6 +76,8 @@ def login():
         return jsonify({"msg": "Bad email or password"}), 401
     access_token = create_access_token(identity=email)
     return jsonify(access_token=access_token)
+
+# any other endpoint will try to serve it like a static file
 @api.route('/register', methods=['POST'])
 def crear_Usuario():
     request_body = request.json #Guardo la respuesta que trae la solicitud en una variable que se llama "request_body" que es un objeto
@@ -74,6 +90,8 @@ def crear_Usuario():
         return jsonify({"msg":"Nuevo usuario ha sido creado"}), 200
     else:
         return jsonify({"msg":"El email ya esta registrado."}), 400
+
+# any other endpoint will try to serve it like a static file
 @api.route('/product/<int:costumer_id>/<int:id>', methods=['DELETE'])
 def delete_product(costumer_id,id):
     request_body=request.json
@@ -86,7 +104,11 @@ def delete_product(costumer_id,id):
     db.session.delete(query)
     db.session.commit()
     return jsonify({"msg":"El producto seleccionado ha sido eliminado correctamente"}),
-#obteniendo info de todos los productos
+
+
+# ----------------------------------------------------------- Productos--------------------------------- #
+
+#--- 1) obteniendo info de todos los productos ---
 @api.route('/products', methods=['GET'])
 def handle_products():
     allproducts = Product.query.all()
@@ -94,7 +116,8 @@ def handle_products():
     results = list(map(lambda item: item.serialize(),allproducts))
     print(results)
     return jsonify(results), 200
-#obteniendo info de todos los productos
+
+#--- 2) obteniendo info de todos los productos ---
 @api.route('/editprofile', methods=['GET'])
 def handle_profile():
     profile = Costumer.query.all()
@@ -102,40 +125,45 @@ def handle_profile():
     results = list(map(lambda item: item.serialize(),profile))
     print(results)
     return jsonify(results), 200
-#RECUPERACION CONTRASEÑA OLVIDADA
+
+# ----------------------------------------------------------- RECUPERACION CONTRASEÑA OLVIDADA --------------------------------- #
 @api.route("/forgotpassword", methods=["POST"])
 def forgotpassword():
     recover_email = request.json['email']
     recover_password = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(8))
-    #clave aleatoria nueva
+    # 1) clave aleatoria nueva 
     if not recover_email:
         return jsonify({"msg": "Debe ingresar el correo"}), 401
-    #busco si el correo existe en mi base de datos
+    #2) busco si el correo existe en mi base de datos
     costumer = Costumer.query.filter_by(email=recover_email).first()
     if recover_email != costumer.email:
         return jsonify({"msg": "El correo ingresado no existe en nuestros registros"}), 400
-    #si existe guardo la nueva contraseña aleatoria
+    #3) si existe guardo la nueva contraseña aleatoria
     costumer.password = recover_password
     db.session.commit()
-    #luego se la envio al usuario por correo para que pueda ingresar
+    #4) luego se la envio al usuario por correo para que pueda ingresar
     msg = Message("Hi", recipients=[recover_email])
     msg.html = f"""<h1>Su nueva contraseña es: {recover_password}</h1>"""
     current_app.mail.send(msg)
     return jsonify({"msg": "Su nueva clave ha sido enviada al correo electrónico ingresado"}), 200
-#obteniendo info de todos los favoritos
+
+# ----------------------------------------------------------- Favoritos --------------------------------- #
+#--- 1) Obteniendo info de todos los favoritos ---
 @api.route('/costumer/<int:costumer_id>/favorites', methods=['GET'])
 def handle_favorites(costumer_id):
     allfavorites = Favorites.query.filter_by(costumer_id=costumer_id).all()
     results = list(map(lambda item: item.serialize(),allfavorites))
     return jsonify(results), 200
-#obteniendo un favorito
+
+#--- 2) Obteniendo un favorito ---
 @api.route('/costumer/<int:costumer_id>/favorites/<int:favorites_id>', methods=['GET'])
 def single_favorite(costumer_id, favorites_id):
     singlefavorite = Favorites.query.filter_by(costumer_id=costumer_id, id=favorites_id).first()
     if singlefavorite is None:
         raise APIException('El producto no existe', status_code=404)
     return jsonify(singlefavorite.serialize()), 200
-# #agregando productos a favoritos
+
+#--- 3) agregando productos a favoritos ---
 @api.route("/costumer/<int:costumer_id>/favorites/product", methods=["POST"])
 def add_favorites(costumer_id):
     request_body = request.get_json()
@@ -148,7 +176,8 @@ def add_favorites(costumer_id):
         return jsonify("Producto añadido"), 200
     else:
         return jsonify("Este producto ya existe"), 400
-# #eliminando productos de favoritos
+
+#--- 4) eliminando productos de favoritos ---
 @api.route("/costumer/<int:costumer_id>/favorites", methods=["DELETE"])
 def del_favorites(costumer_id):
     request_body = request.get_json()
@@ -158,7 +187,10 @@ def del_favorites(costumer_id):
     db.session.delete(favorites)
     db.session.commit()
     return jsonify("El producto ha sido eliminado"), 200
-#obtener info de customer
+
+# ----------------------------------------------------------- Costumer --------------------------------- #
+
+#--- 1) Obtener info de customer ---
 @api.route('/register', methods=['GET'])
 def handle_costomer():
     allcostumer = Costumer.query.all()
@@ -167,39 +199,34 @@ def handle_costomer():
     print(results)
     return jsonify(results), 200
 
-#obtener info category todos
+#--- 2) obtener info category todos ---
 @api.route('/category', methods=['GET'])
 def handle_category():
     allcategory = Category.query.all()
     print(allcategory)
     results = list(map(lambda item: item.serialize(),allcategory))
     print(results)
-
     return jsonify(results), 200
 
-#Obteniendo el producto dentro de una categoria
+#--- 3) Obteniendo el producto dentro de una categoria---
 @api.route('/category/<int:category_id>/products/', methods=['GET'])
 def get_products_category(category_id):
     
     category_products = Product.query.filter_by(category_id=category_id).all()
     results = list(map(lambda item: item.serialize(),category_products))
-
     return jsonify(results), 200
 
-#obtener info category solo 1
+#--- 4) obtener info category solo 1 ---
 @api.route('/category/<int:category_id>', methods=['GET'])
 def handle_category_varios(category_id):
     category_one = Category.query.filter_by(id=category_id).first()
-
     return jsonify(category_one.serialize()), 200
 
-#obteniendo info de un solo product
+#--- 4) Obteniendo info de un solo product---
 @api.route('/product/<int:product_id>', methods=['GET'])
-def get_info_product(product_id):
-    
+def get_info_product(product_id): 
     product = Product.query.filter_by(id=product_id).first()
     return jsonify(product.serialize()), 200
-
 
 
 
