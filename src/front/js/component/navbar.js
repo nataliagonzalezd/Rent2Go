f3761ea157b0d3dbc987d11f1d5f87bcba438925
Valentina Pereyra Castillo <valepereyracastillo@gmail.com>
2023-02-21 17:22:01 +0000

    navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -2,6 +2,10 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import "../../styles/navbar.css";
+import Diseño from "../../img/Diseño.png";
+
+
 export const Navbar = () => {
   const { store, actions } = useContext(Context);
   const navigate = useNavigate();
@@ -10,174 +14,114 @@ export const Navbar = () => {
     navigate("/login");
   }
   return (
-    <>
-      <div className="bg-white border-bottom border-dark">
-        <nav className="navbar bg-white d-flex">
-          <div className="container-fluid d-flex bg-white">
-            <a className="navbar-brand d-flex justify-content-start align-items-center">
-              <img
-                src="https://i.pinimg.com/564x/aa/80/36/aa80366b7098c9cebc7f47b7614a2176.jpg"
-                className="w-25 h-25"
-                alt=""
-              />{" "}
-              Reant2go
-            </a>
-            <input
-              className="rounded-pill form-control w-50 mx-auto bg-light"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            ></input>
-          </div>
-          <div className="d-flex justify-content-start">
-            <ul className="nav grid gap-5">
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle text-black"
-                  href="#"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Caregorias
+    <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top" id="navbar">
+      <div className="container-fluid">
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+          <i className="fa fa-solid fa-bars"></i>
+        </button>
+        <a className="navbar-brand text-light fs-4" href="/"> <img src={Diseño} width="38" height="38" className="border-roudend" alt="" />Rent2Go</a>
+
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <div className="dropdown-center">
+                <a className="nav-link dropdown-toggle text-light" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  PRODUCTOS
                 </a>
-                <ul className="dropdown-menu text-black">
+                <ul className="dropdown-menu">
+                  <li><span className="dropdown-item-text">CATEGORIAS</span></li>
+                  <li><a
+                    className="dropdown-item text-black"
+                    href="/category/1/products"
+                  >
+                    Pintura
+                  </a></li>
+                  <li> <a
+                    className="dropdown-item text-black"
+                    href="/category/2/products"
+                  >
+                    Electronica
+                  </a></li>
+                  <li><a
+                    className="dropdown-item text-black"
+                    href="/category/3/products"
+                  >
+                    Jardineria
+                  </a></li>
+                  <li><a
+                    className="dropdown-item text-black"
+                    href="/category/4/products"
+                  >
+                    Indumentaria
+                  </a></li>
+                  <li><a
+                    className="dropdown-item text-black"
+                    href="/category/5/products"
+                  >
+                    Musica
+                  </a></li>
+                  <li><a
+                    className="dropdown-item text-black"
+                    href="/category/6/products"
+                  >
+                    Lectura
+                  </a></li>
+                </ul>
+              </div>
+
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-light" href="#">OFERTAS</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-black" href="/newProduct">
+                Subir un producto
+              </a>
+            </li>
+          </ul>
+          <form className="d-flex mx-5" role="search">
+            <input className="form-control me-2 rounded-pill" type="search" placeholder="Buscar productos.." aria-label="Search" />
+          </form>
+          <ul className="navbar-nav">
+
+
+            <li className="nav-item dropdown">
+              <div className="btn-group dropstart">
+                <a className="nav-link dropdown-toggle active text-light" data-bs-display="static" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  <i className="fa fa-regular fa-user"></i>
+                </a>
+                <ul className="dropdown-menu dropdown-menu-end dropdown-menu-lg-start">
+                  <li><a className="dropdown-item" href="/login">Ingresa</a></li>
+                  <li><a className="dropdown-item" href="/register">Crea tu cuenta</a></li>
+                  <li><a className="dropdown-item" href="#">Mi cuenta</a></li>
                   <li>
-                    <a className="dropdown-item text-black" href="/">
-                      Home
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      className="dropdown-item text-black"
-                      href="/category/1/products"
-                    >
-                      Pintura
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      className="dropdown-item text-black"
-                      href="/category/2/products"
-                    >
-                      Electronica
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      className="dropdown-item text-black"
-                      href="/category/3/products"
-                    >
-                      Jardineriaa
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      className="dropdown-item text-black"
-                      href="/category/4/products"
-                    >
-                      Indumentaria
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      className="dropdown-item text-black"
-                      href="/category/5/products"
-                    >
-                      Musica
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      className="dropdown-item text-black"
-                      href="/category/6/products"
-                    >
-                      Lectura
-                    </a>
-                  </li>
-                  <li>
-                    <hr className="dropdown-divider text-black"></hr>
-                  </li>
-                  <li>
-                    <a className="dropdown-item text-black" href="">
-                      Favoritos
-                    </a>
+                    {store.auth === true ? (
+                      <button className="btn" onClick={handleLogout}>
+                        Salir
+                      </button>
+                    ) : null}
                   </li>
                 </ul>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link active text-black"
-                  aria-current="page"
-                  href=""
-                >
-                  Ofertas
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link text-black" href="">
-                  Marcas
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link text-black" href="">
-                  Historial
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link text-black" href="/newProduct">
-                  Subir un producto
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div className="d-flex justify-content-start">
-            <ul className="nav grid gap-1">
-              <li className="nav-item">
-                <a
-                  className="nav-link active text-black"
-                  aria-current="page"
-                  href="/register"
-                >
-                  Register
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link active text-black"
-                  aria-current="page"
-                  href="/login"
-                >
-                  Login
-                </a>
-              </li>
-              {store.auth === true ? (
-                <button className="btn btn-dark" onClick={handleLogout}>
-                  Logout
-                </button>
-              ) : null}
-              <li className="nav-item">
-                <button
-                  className="nav-link active text-black"
-                  aria-current="page"
-                  href="viewcart"
-                >
-                  Carrito
-                </button>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link active text-black"
-                  aria-current="page"
-                  href="viewfavorites"
-                >
-                  Favoritos
-                </a>
-              </li>
-            </ul>
-          </div>
-        </nav>
+
+              </div>
+
+            </li>
+
+            <li className="nav-item">
+              <a className="nav-link active text-light mx-2" aria-current="page" href="viewcart">
+                <i className="fa fa-solid fa-cart-plus"></i>
+              </a>
+            </li>
+
+            <li className="nav-item">
+              <a className="nav-link active text-light" href="viewfavorites">
+                <i className="fa fa-regular fa-heart"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+
       </div>
-    </>
+    </nav>
   );
 };
