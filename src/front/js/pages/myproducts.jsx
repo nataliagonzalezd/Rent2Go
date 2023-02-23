120d1ecd39660ea2ca75f9b2c250072e1c559c51
Valentina Pereyra Castillo <valepereyracastillo@gmail.com>
2023-02-23 15:11:50 +0000

    boton dashboard

--- src/front/js/pages/myproducts.jsx
+++ src/front/js/pages/myproducts.jsx
@@ -1,15 +1,19 @@
 import React, { useContext, useEffect } from "react";
 import { Context } from "../store/appContext";
 import MyProducts from "../component/myproducts.jsx";
+import { Navigate } from "react-router-dom";
 
 const MyProductsView = () => {
   const { store, actions } = useContext(Context);
 
+
   useEffect(() => {
-    actions.getProductsDetails();
+    actions.getProductsDetails(localStorage.getItem("costumer_id"));
   }, [store.products]);
 
-  return (
+  return localStorage.getItem("costumer_id") === null ? (
+    <Navigate to="/login" />
+  ) : (
     <div className="container-fluid row g-4 my-2 d-flex justify-content-center">
       <div className="col-12">
         {store.products.map((cadaProducto, index) => (
