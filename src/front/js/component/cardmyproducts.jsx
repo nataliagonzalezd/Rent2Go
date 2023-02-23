78b05124100ff3035be1edeffced9c4de31279d1
nataliagonzalezd <nataliagd11@hotmail.com>
2023-02-23 02:04:45 +0000

    Creando Dashboard y Mostrando productos, creacion del PUT para actualizar productos

---
+++ src/front/js/component/cardmyproducts.jsx
@@ -0,0 +1,39 @@
+import React, { useContext, useEffect } from "react";
+import { Context } from "../store/appContext";
+import { Link } from "react-router-dom";
+//create your first component
+const CardProduct = (props) => {
+  const { store, actions } = useContext(Context);
+
+  useEffect(() => {
+    actions.getProductsDetails();
+  }, []);
+
+  return (
+    <div className="card">
+      <div className="mails">
+        <img src={props.image} />
+        <div className="mail-names">{props.name}</div>
+      </div>
+      <div className="mail-info">{props.description}</div>
+      <div></div>
+      <div className="bottom-info">
+        <div className="check1">
+          <i className="fa fa-solid fa-pen"></i>
+          <div>
+            <Link
+              to={"/costumer/" + props.costumer_id + "/editproduct/" + props.id}
+            />
+            <i className="fa fa-solid fa-trash"></i>
+          </div>
+          <div className="star">
+            <i className="fa-solid fa-pen-to-square"></i>
+          </div>
+        </div>
+        <div className="date"></div>
+      </div>
+    </div>
+  );
+};
+
+export default CardProduct;
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -287,9 +287,34 @@ const getState = ({
                     console.log(error);
                 }
             },
-
-            /////////////////////////////////////////////////////////////
-
+            updateProduct: (id) => {
+                fetch(process.env.BACKEND_URL + "/api/product/" + id, {
+                        method: 'PUT',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({
+                            name: "name",
+                            description: "jaja",
+                            price: 89,
+                        })
+                    })
+                    .then(response => {
+                        if (response.ok) {
+                            return response.json();
+                        } else {
+                            throw new Error('Error al actualizar el producto.');
+                        }
+                    })
+                    .then(data => {
+                        console.log(data);
+                        return data;
+                    })
+                    .catch(error => {
+                        console.log(error.message);
+                        throw error;
+                    });
+            },
             getProductsCategory: (category_id) => {
                 fetch(
                         process.env.BACKEND_URL +
